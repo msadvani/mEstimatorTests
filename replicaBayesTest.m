2955f51125167461880a76c923aa5f1effde7de5
Madhu <m.advani81@gmail.com>
2013-12-24 17:21:06 -0500

    testing replica equations for L2 norm in both rho and sigma.

---
+++ replicaBayesTest.m
@@ -0,0 +1,64 @@
+clear all;
+close all;
+
+lambda = 6;
+kappa = .5;
+
+
+b = 1-kappa-kappa*lambda;
+c = -kappa*lambda;
+ 
+d = (-b+sqrt(b^2-4*c))/2;
+
+ 
+qThy  = @(k) ((d^2 + k)./((1+d)^2 - k));
+
+
+%% Simulation
+n=500;
+p = round(n*kappa);
+%s = 1;
+
+
+
+
+numSim = 5;
+qErr = zeros(1,numSim);
+for cnt = 1:numSim
+    X = (1/sqrt(p))*randn(n,p); %random unit normal design matrix
+    beta0 = randn(p,1);
+
+    %sign = 2*round(rand(n,1))-1;
+    %epsilon = sign.*exprnd(s,n,1);  %double exponential
+    epsilon = randn(n,1);
+    
+    y = X*beta0 + epsilon;
+
+    
+    
+    
+    bMin = fminunc(@(b)(norm(y-X*b,2)^2 + lambda*norm(b,2)^2),randn(p,1));
+
+    qErr(cnt) = (1/p)*norm(bMin - beta0,2)^2;
+      
+%       rho = @(x)(1/2)*x.^2;
+%       
+%       %sigma = @(x)zeros(size(x)); %Easy case
+%       sigma = @(x) (1/2)*lambda*(x.^2);
+%       
+%       tic
+%       [bMinS]=fminunc(@(b)sum(rho(y-X*b)), randn(p,1));
+%       %[bMinS] = fminunc(@(b) sum(rho(y-X*b)) + sum(sigma(b)),randn(p,1));
+%       toc
+%       
+%       rhoErr(cnt) = (1/sqrt(P))*norm(bMinS - beta0,2);
+end
+
+
+disp('Numerical Experiment: 60% confidence interval')
+[mean(qErr)-std(qErr),mean(qErr)+std(qErr)]
+
+
+disp('Theory')
+qThy(kappa)
+
